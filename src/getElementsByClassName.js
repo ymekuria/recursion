2f0164d54fcb3b5640204fe8726663a2ae740bc7
Yoni <ymekuria@gmail.com>
2015-10-25 22:50:36 -0700

    'First commit. Returns the elements with the target className'

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,46 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/*
+Returns an array-like object of all child elements which have all of the given class names. When called on the document object, the complete document is searched, including the root node. You may also call getElementsByClassName() on any element; it will return only elements which are descendants of the specified root element with the given class names.
+
+*/
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className) {
+
+  
+  //Push the nodes with matching classes here. 
+  var elementList = [];
+
+
+  //iterating through the child nodes of the body.
+  console.log("this.document: ", this.document.body, "document, :", document.body);
+  console.log("this.document.body.children: ", document.body.childNodes);  
+
+  for(var i = 0;i < this.document.body.children.length; i++) {
+    
+    //if the node has a classlist, then compare each value to the target passed in.
+    if(document.body.children[i].classList.length > 0) {
+
+      for(var j = 0; j < document.body.children[i].classList.length; j++) {
+
+        //This variable references each value in the classList array.
+        var objClass = document.body.children[i].classList[j];
+
+        //If the value = the passed in target, the element is pushed to the elementList array.
+        if(objClass === className) {
+          var elem = document.body.children[i];
+          elementList.push(elem);
+        }
+      }
+    }
+  }
+  
+    return elementList;
+  };
+
+		
+
+
+
