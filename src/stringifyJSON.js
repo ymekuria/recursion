65ee915199edc41fdf7bd7d6abd63fb05b5d6e72
Yoni <ymekuria@gmail.com>
2015-10-26 21:05:22 -0700

    'Created a case for functions/undefined values and passed all tests.'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,57 +3,65 @@
 
 // but you don't so you're going to write it from scratch:
 
-  var addToString = function(objToString) {
-    //console.log("test object directly inside addToString: ", objToString, "objToString.length: ", objToString.length = true)
+var stringifyJSON = function(obj) {
+
+  //This string will be returned with the stringified values. 
+  var objString="";
+  
+    //This function turns the passed in arguments to a string and adds it to the objString.
+    var addToString = function(objToString) {
+      
+      //This conditinal tests if the passed in argument is an Array.
+      if (Array.isArray(objToString)) {
+        objString += "[";
         
-        //This conditinal tests if the passed in argument is an Array
-
-
-        if(Array.isArray(objToString)) {
-          objString += "[";
-          
-          //If so, iterate through the array and recursively call the addtoString on each value         
-          _.each(objToString, function(arrValue, index) {
-            //objString += (keyInValue+":")
-
-            console.log("objString value inside of each for Array conditional: ", arrValue, "value type inside of each: ", typeof arrValue )
-            addToString(arrValue);
-          });
-          
-         
-          //This deletes the last comma after the last stringified element in a collection. 
-          objString.charAt(objString.length-1) === "," ? objString = objString.slice(0, - 1) : objString;
-          
-          objString += "],";
-        }
-
-        //If the passed in argument is a string, add it to the objString.
-        else if(typeof objToString === "string") {
-          //Escape the string
-           objString += objToString;
-
-        }
+        //If so, iterate through the array and recursively call the addtoString on each value.         
+        _.each(objToString, function(arrValue, index) {
+          addToString(arrValue);
+        });
         
-        //If the is converted to a string and added to the objString if it's a number, boolean, or null.
-        else if(typeof objToString === "boolean" || typeof objToString ===  "number" || typeof objToString === null) {
-          console.log
-          //objString += objToString.toString();
-          objString = objString.concat(objToString.toString(),",");
-        }
+        //This deletes the last comma after the last stringified element in a collection. 
+        objString.charAt(objString.length-1) === "," ? objString = objString.slice(0, - 1) : objString;
         
-        //If the argument is an object that isn't an array.
-        else if(typeof objToString === "object" && !Array.isArray(objToString)) {
-          objString += "{";
-          _.each(objToString, function(objValue, key) {
+        objString += "],";
+      }
 
-            //Each key is added to the objString and the value is passed in to the addToString function.
-            objString += (key+":")
-            console.log('key inside of object _.each: ', key, 'objValue: ', objValue);
-            addToString(objValue);
-          });
-          objString += "}";
-        }
-   
+      //If the passed in argument is a string, add it to the objString.
+      else if (typeof objToString === "string") {  
+         objString += '"'+objToString+'",';
+      }
+      
+      //If the argument is converted to a string and added to the objString if it's a number or boolean.
+      else if (typeof objToString === "boolean" || typeof objToString ===  "number") {
+        objString = objString.concat(objToString.toString(),",");
+      }
+
+      //If the argument is null, the string 'null' is addeded to the objString.
+      else if (objToString === null) {
+        objString+="null,";
+      }
+
+      //If the argument is an object that isn't an array.
+      else if (typeof objToString === "object" && !Array.isArray(objToString)) {
+        objString += "{";
+
+        _.each(objToString, function(objValue, key) {
+          
+          //This conditional determines if the argument is stringyfiable (ie not undefined or a function). 
+          if (typeof objValue !== 'function' && objValue !== undefined) {
+           
+          //Each key is added to the objString and the value is passed in to the addToString function.
+          objString += ('"'+key+'":');
+          
+          addToString(objValue);
+          }
+        });
+
+        //This deletes the last comma after the last stringified element in a collection.
+        objString.charAt(objString.length-1) === "," ? objString = objString.slice(0, - 1) : objString;
+        objString += "},";
+      }
+ 
   };      
   
   //Initially invoking addToString with the obj passed into the stringify function.
